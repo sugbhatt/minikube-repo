daf682ca57315f17aef052cf523e4a5b2548f2bd
Bhattacherjee <683211@cognizant.com>
2018-07-18 13:59:54 +0530

    add all files

---
+++ src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    
+    @RequestMapping("/")
+    public String greeting() throws UnknownHostException{
+
+        double x = 0.0001;
+        for (int i = 0; i <= 1000000; i++) {
+            x += Math.sqrt(x); 
+        }
+        return "Hello Boot! Date: " + new SimpleDateFormat("yyyy-MM-dd:HH.mm.ss").format(new Date()) + " from Host: " + InetAddress.getLocalHost().getHostName(); 
+    }
+  
+}
